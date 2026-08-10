ff49faf428a9df39fd817f8dcef67ff9fc0d6fdf
Akash <wagandhawarodrigo@gmail.com>
2026-08-10 22:49:08 +0530

    fixed merge conflict of application form

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ApplicationForm from "../components/ApplicationForm";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-zinc-800 py-12 px-4">
-       <ApplicationForm />
+    <main className="min-h-screen bg-zinc-800 py-3 px-4">
+      <ApplicationForm />
     </main>
   );
 }
